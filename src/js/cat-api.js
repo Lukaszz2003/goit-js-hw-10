d02996a7426c07fe6c3c7b5d1a6e8f3af9a9b702
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-01 21:40:22 +0200

    new

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,3 +5,7 @@ import 'slim-select/dist/slimselect.css';
 
 axios.defaults.headers.common['x-api-key'] =
   live_97xII2qZqQff8ME2anlv4JVHUVR8D0EpoBLgZP3t8bYMvJqeN0BvS2sqI7nRVhSK;
+
+const element = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
