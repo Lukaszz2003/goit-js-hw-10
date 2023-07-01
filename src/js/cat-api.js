44c70066ad1ea54a123c588e488461d5a1027b7b
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-01 21:37:43 +0200

    new

---
+++ src/js/cat-api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+axios.defaults.headers.common['x-api-key'] =
+  live_97xII2qZqQff8ME2anlv4JVHUVR8D0EpoBLgZP3t8bYMvJqeN0BvS2sqI7nRVhSK;
