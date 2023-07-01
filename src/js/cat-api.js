6fd187c9daf0e48375389d4e0d114b13d1ac277b
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-01 22:28:14 +0200

    new

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -70,3 +70,73 @@ fetchBreeds()
       'Oops! Something went wrong! Try reloading the page!'
     );
   });
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      noElement(loader);
+    });
+}
+
+function renderCatInfo(cat) {
+  const image = cat[0].url;
+  const name = cat[0].breeds[0].name;
+  const description = cat[0].breeds.description;
+  const temperament = cat[0].breeds.temperament;
+
+  const catMarkup = `
+    <img class ="card__image" src="${image}" alt="${name} image">
+    <div class="card__content">
+        <h2 class="card__heading">${name}</h2>
+        <p class="card__description">${description}</p>
+        <p class="card__text"><span class="bold">Temperament: </span>${temperament}</p>
+    </div>
+  `;
+
+  catInfo.innerHTML = catMarkup;
+}
+
+element.addEventListener('change', () => {
+  const selectBreed = element.value;
+  if (selectBreed !== '') {
+    yesElement(loader);
+    noElement(catInfo);
+    fetchCatByBreed(selectBreed)
+      .then(cat => {
+        renderCatInfo(cat);
+        noElement(loader);
+        yesElement(catInfo);
+      })
+      .catch(error => {
+        console.log(error);
+        Notiflix.Notify.failure(
+          'Oops! Something went wrong! Try reloading the page!'
+        );
+      });
+  }
+});
+
+function noElement(element) {
+  element.classList.add('hidden');
+}
+
+function yesElement(element) {
+  element.classList.remove('hidden');
+}
+
+function addStyles() {
+  element.classList.add('slim-select');
+}
