191ecaa8405d35f1546d34a83d07e04534edf82a
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-02 10:32:54 +0200

    new

---
+++ src/cat-api.js
@@ -0,0 +1,36 @@
+const API_KEY =
+  'live_97xII2qZqQff8ME2anlv4JVHUVR8D0EpoBLgZP3t8bYMvJqeN0BvS2sqI7nRVhSK';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds`, { 'x-api-key': `${API_KEY}` }).then(
+    result =>
+      result.json().then(data => {
+        return data;
+      })
+  );
+}
+
+function fetchCatByBreed(id) {
+  return fetch(
+    `${BASE_URL}/images/search?&api_key=${API_KEY}&breed_ids=${id}`
+  ).then(result =>
+    result.json().then(data => {
+      // GET PIC
+      const dataName = data[0].breeds[0];
+
+      const catInfo = {
+        pic: data[0].url,
+        name: dataName.name,
+        desc: dataName.description,
+        temper: dataName.temperament,
+        id: dataName.id,
+      };
+
+      return catInfo;
+    })
+  );
+}
+
+export { fetchBreeds, fetchCatByBreed };
